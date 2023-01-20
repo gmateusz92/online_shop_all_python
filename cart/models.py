69cb74526e086cf07411b053fab8a3bfba06b1d8
gmateusz92 <gmateusz92@gmail.com>
2023-01-20 12:56:12 +0100

    1

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from store.models import Products
 
-# Create your models here.
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, null=True, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product.product
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cart, Products, CartItem
 
-# Create your views here.
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def cart(request):
+    #cart = Cart.objects.get(cart_id=_cart_id(request))
+    #cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+    return render(request, 'cart.html')
+
+def add_cart(request, id):
+    cart = Cart.objects.all()
+    product = Products.objects.get(id=id)
+    cart.add(product)
+    return redirect("index")
