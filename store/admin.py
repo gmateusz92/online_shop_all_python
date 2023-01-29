86a12e28ebe6d19cf281ff5a59f5b0a83f2faaad
gmateusz92 <gmateusz92@gmail.com>
2023-01-29 13:50:29 +0100

    1

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from .models import Products, Category
+
 from django.utils.html import format_html
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'category'] #wyswietla w panelu admina
+    search_fields = ['title']
+    list_filter = ['title']
 
     def image_tag(self, obj): #wyswietla maly obrazek itemu w panelu admina
         return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.image.url))
+
     image_tag.short_description = 'Product Image Preview'
     readonly_fields = ['image_tag']
 
 admin.site.register(Category)
 admin.site.register(Products, ProductAdmin)
 
+
+admin.site.site_header = "SHOP Dashboard"
+admin.site.site_title = "SHOP"
+admin.site.index_title = "Welcome to SHOP Dashboard"
